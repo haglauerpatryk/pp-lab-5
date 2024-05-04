8abeb5e2eccf78901d4e51f1448f0545c466c81b
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-05-04 09:16:33 +0200

    Zadanie 2

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import company.beans.Person;
 import company.exceptions.InvalidAgeException;
+import company.implementations.EmailMessenger;
+import company.interfaces.Messenger;
 
 public class Main {
     public static void main(String[] args) {
@@ -7,6 +9,9 @@ public class Main {
             Person person = new Person("John", 30);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
+
+            Messenger messenger = new EmailMessenger();
+            messenger.sendMessage("Hello from EmailMessenger!");
         } catch (InvalidAgeException e) {
             System.out.println("Invalid age: " + e.getMessage());
         }

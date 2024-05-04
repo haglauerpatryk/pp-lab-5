7b0d1323021d14c247cecb6e45de756252cc1013
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-05-04 09:21:24 +0200

    Zadanie 3

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ import company.beans.Person;
 import company.exceptions.InvalidAgeException;
 import company.implementations.EmailMessenger;
 import company.interfaces.Messenger;
+import company.utils.MathUtils;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,6 +10,9 @@ public class Main {
             Person person = new Person("John", 30);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
+
+            int result = MathUtils.add(5, 7);
+            System.out.println("Result of addition: " + result);
 
             Messenger messenger = new EmailMessenger();
             messenger.sendMessage("Hello from EmailMessenger!");

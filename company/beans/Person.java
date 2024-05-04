dff57bb41ec8e7a9a44e385d37d69975ca5483e8
haglauerpatryk <138967892+haglauerpatryk@users.noreply.github.com>
2024-05-04 09:10:23 +0200

    Zadanie 1

---
+++ company/beans/Person.java
@@ -0,0 +1,33 @@
+package company.beans;
+
+
+import company.exceptions.InvalidAgeException;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) throws InvalidAgeException {
+        setName(name);
+        setAge(age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) throws InvalidAgeException {
+        if (age < 0 || age > 125) {
+            throw new InvalidAgeException("Invalid age. Age must be between 0 and 125.");
+        }
+        this.age = age;
+    }
+}
